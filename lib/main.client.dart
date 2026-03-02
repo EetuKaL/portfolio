af62266c3c85e2b8de5d17da51e1b21a21895284
EetuKaL <89065242+EetuKaL@users.noreply.github.com>
2026-03-02 14:21:06 +0200

    refactor app's entry point and added dialog component

---
+++ lib/main.client.dart
@@ -0,0 +1,13 @@
+// Client-specific Jaspr import.
+import 'package:jaspr/client.dart';
+
+// This file is generated automatically by Jaspr, do not remove or edit.
+import 'main.client.options.dart';
+
+void main() {
+  // Initializes the client environment with the generated default options.
+  Jaspr.initializeApp(options: defaultClientOptions);
+
+  // Starts running the app.
+  runApp(ClientApp());
+}
--- lib/main.server.dart
+++ lib/main.server.dart
@@ -1,9 +1,29 @@
+import 'package:eetu_portfolio/app.dart';
+import 'package:jaspr/dom.dart';
 import 'package:jaspr/server.dart';
+import 'package:jaspr_localizations/jaspr_localizations.dart';
 
-import 'app.dart';
 import 'main.server.options.dart';
 
 void main() {
+  final locale = getCurrentLocale();
   Jaspr.initializeApp(options: defaultServerOptions);
-  runApp(App());
+  runApp(
+    Document(
+      title: 'Eetu Kallioniemi',
+      styles: [css.import('./styles.css')],
+      meta: {'viewport': 'width=device-width, initial-scale=1.0'},
+      head: [
+        script(src: 'https://cdn.tailwindcss.com'),
+        link(
+          rel: 'stylesheet',
+          href:
+              'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css',
+        ),
+        script(src: 'app.js'),
+      ],
+      body: App(),
+      lang: locale.languageCode,
+    ),
+  );
 }
